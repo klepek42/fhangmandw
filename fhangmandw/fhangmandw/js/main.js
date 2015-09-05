8d410381892313f3de8d43808e74f8b8d29ab8cb
edgar_klepek <edgarklepek@gmail.com>
2015-09-06 00:39:36 +0200

    Version 1.3 ABC-Tastatur und Ratewortfeld
    Die Funktionen createButtons() und fillSecret() in Kombination mit
    randomWord() und jQuery implementiert. Der Button "nächstes Wort"
    generiert jetzt onclick immer neues Wort und baut das Ratewortfeld neu
    auf.

--- fhangmandw/fhangmandw/js/main.js
+++ fhangmandw/fhangmandw/js/main.js
@@ -1,8 +1,20 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var words = ["fhdw", "informatik", "bwl"];
+var words = ["fhdw", "informatik", "bwl", "dual", "studium", "finanzmathematik", "bilanz", "guv", "gewinn", "javascript", "java", "software", "wirtschaft", "klausur", "praesentation", "office", "praxis"];
 var word;
 var lives;
-var points;
+var points = 100;
+
+//Erzeugen der Buttons mit Buchstaben
+var createButtons = function() {
+	jQuery(function($){
+		for(var i = 0; i < letters.length; i++) {
+			var letter = letters[i];
+			$("#buttons ul").append("<li>" + letter + "</li>");
+			
+		}
+	});
+}
+createButtons();
 
 //Waehle zufaelliges Wort aus Woerterarray
 function randomWord() {
@@ -10,36 +22,30 @@ function randomWord() {
 	console.log("randomWord = " + word);
 	return word;
 }
-
 randomWord();
 
-//Clevere Funktion zum erzeugen der Buttons mit Buchstaben
-var createButtons = function() {
-	for(var i = 0; i < letters.length; i++) {
-		//Ansatz ungefaehr in diesem Stil
-		var addBtns = document.createElement('li');
-		buttons.appendChild('letters');
-	}
-}
-
-//createButtons();
-
 //Befüllen des Ratefelders mit Elementen in Abhängigkeit des zufällig gewählten Wortes
-var secretArea = function() {
-	for(var j = 0; j < word.length-1; j++) {
-
-	}
+var fillSecret = function() {
+	jQuery(function($){
+	//Reset fuer "nächstes Wort"
+	$("#area ul").empty();
+		for(var j = 0; j < word.length; j++) {
+		//Erstelle so viele Underscores wie die Wortlänge
+			$("#area ul").append('<li>_</li>');
+		}
+	});
 }
+fillSecret();
 
 //Pruefen ob der Benutzer einen richtigen Buchstaben gewählt hat
 //Wenn richtig dann decke auf und tausche _ durch richtigen Buchstaben
 var checkLetter = function() {
 	if(letters[x] === letter) {
-		;
 	}
 }
 
 //eventListener bzw. onclick Event für die Buchstaben Buttons -> ausführen von checkLetters()
 
+//Tastatureingaben
 
 //Idee: Punktevergabe - Richtiger Buchstabe = 100 Punkte - Falscher Buchstabe -25 ?
