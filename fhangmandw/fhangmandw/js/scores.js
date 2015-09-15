737922948c8f6e021f6016c5638a7e32f0e6410b
edgar_klepek <edgarklepek@gmail.com>
2015-09-15 19:48:04 +0200

    Version 1.9.3
    Gameoverseite überarbeitet und Bilder/Gif zum Gameplay hinzugefügt.

--- fhangmandw/fhangmandw/js/scores.js
+++ fhangmandw/fhangmandw/js/scores.js
@@ -3,14 +3,14 @@ var schoolname;
 var highscores = [];
 
 jQuery(function($){
-    createHighscorelist();
     $('#results').hide();
-    $('#results').show("drop", 1200);
-
+    $('#results').show("fade", 2000);
+    createHighscorelist();
     $('#results').click(function() {
     	$('#results').toggle("fade");
   		setTimeout(function () {
                location.href = 'index.html';
         }, 500);
   	});
+    $('#back').toggle("pulsate", { times: 80 }, 50000);
 });
