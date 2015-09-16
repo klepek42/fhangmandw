f8dbf487f98369696731d6c884146f96595f5ebc
edgar_klepek <edgarklepek@gmail.com>
2015-09-16 13:57:48 +0200

    Version 1.9.7
    Credits im Menü hinzugefügt mit einem SweetAlert. Hinweistext in
    Instructions mittig positioniert für garantierte Sicht.

--- fhangmandw/fhangmandw/js/init.js
+++ fhangmandw/fhangmandw/js/init.js
@@ -35,6 +35,13 @@ function init() {
                 location.href = 'instructions.html';
             }, 500);
         });
+
+        $('#attribution').click(function() {
+            swal({   
+                title: "<h1>Credits</h1>",   
+                text: "<h2>freesound.org / CC BY 3.0</h2><ul><li>wrong action.wav by GabrialAraujo</li><li>success_low.wav by grunz</li><li>failfare.mp3 by Wagna</li><li>Menu screen mouse over by DrMinky</li><li>Xylophone-Cartton by sergeeo</li></ul><h2>CC0 1.0</h2><ul><li>game over by fins</li><li>correct by ertfeld</li><li>Game over sounds 1</li><li>Ringing bell by domrodrig</li></ul><h2>flaticon.com</h2><ul><li>Joker Icon made by http://www.freepik.com <br/> from www.flaticon.com</li></ul>",   
+                html: true })
+        });
     });
 }
 
